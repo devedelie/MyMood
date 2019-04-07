0e84769a2ae881d922b1a0b5afaedd4e7f3d20db
e210564e <devedelie@gmail.com>
2019-04-07 20:18:42 +0200

    Creation of new app project Creation of folders for MVC architecture Creation of repository on GitHub

--- app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
+++ app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.elbaz.eliran.mymood;
+package com.elbaz.eliran.mymood.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.elbaz.eliran.mymood.R;
 
 public class MainActivity extends AppCompatActivity {
 
