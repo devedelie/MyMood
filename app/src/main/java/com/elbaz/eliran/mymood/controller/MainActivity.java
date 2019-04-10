b32fb1b658361ff67f4e06db2f1cc66db6642731
e210564e <devedelie@gmail.com>
2019-04-10 15:31:26 +0200

    - Beginning of SWIPE detection code implementation with onFling() - creation of 2nd activity "Really bad mood"

--- app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
+++ app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
@@ -2,14 +2,88 @@ package com.elbaz.eliran.mymood.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import com.elbaz.eliran.mymood.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    public static final int SWIPE_THRESHOLD = 100;
+    public static final int SWIPE_VELOCITY_THRESHOLD = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent downEvent, MotionEvent moveEvent, float velocityX, float velocityY) {
+        boolean result=false;
+        //How far user finger traversed throw the Y axis
+        float diffY = moveEvent.getY() - downEvent.getY();
+        //How far user finger traversed throw the X axis
+        float diffX = moveEvent.getX() - downEvent.getX();
+        //Which was greater? movement across Y or X?
+        if (Math.abs(diffX) > Math.abs(diffY)){
+            //Right or left swipe
+            if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX)> SWIPE_VELOCITY_THRESHOLD){
+                if (diffX >0 ){
+                    onSwipeRight();
+                }else{
+                    onSwipeLeft();
+                }
+            }
+
+        }else{
+            //Up or down swipe
+            if (diffY > 0){
+                onSwipeBottom();
+            }else{
+                onSwipeUp();
+            }
+            
+        }
+
+        return result;
+    }
+
+
+
+    
+    private void onSwipeLeft() {
+    }
+
+    private void onSwipeRight() {
+    }
+    private void onSwipeBottom() {
+    }
+    private void onSwipeUp() {
     }
 }
---
+++ app/src/main/java/com/elbaz/eliran/mymood/controller/ReallyBadMoodActivity.java
@@ -0,0 +1,15 @@
+package com.elbaz.eliran.mymood.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elbaz.eliran.mymood.R;
+
+public class ReallyBadMoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_really_bad_mood);
+    }
+}
