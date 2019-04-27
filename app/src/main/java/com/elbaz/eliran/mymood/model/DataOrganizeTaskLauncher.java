d38ea7ef292334f0ca20e6682b3b14bebf0c1908
e210564e <devedelie@gmail.com>
2019-04-27 18:29:46 +0200

    - Added Renew for AlarmManager: While the alarm will be executed, the broadcast will invoke onReceive(). This action will set a flag to 1 to indicate the system that data should be saved and re-organized. - In addition, a method to check if user is using the app "live" to restart his current mood window. (will be implemented)

--- app/src/main/java/com/elbaz/eliran/mymood/model/DataOrganizeTaskLauncher.java
+++ app/src/main/java/com/elbaz/eliran/mymood/model/DataOrganizeTaskLauncher.java
@@ -3,6 +3,7 @@ package com.elbaz.eliran.mymood.model;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.Toast;
 
 /**
@@ -10,16 +11,30 @@ import android.widget.Toast;
  */
 public class DataOrganizeTaskLauncher extends BroadcastReceiver {
 
-    // This method will be invoked once the AlarmManager reaches the midnight time (00:00:00)
+    // This method will be invoked once the AlarmManager reaches the time we set for data saving
     @Override
     public void onReceive(Context context, Intent intent) {
         // The task to be launched by Android AlarmManager when time is right.
         Toast.makeText(context, "It is midnight. Your data from yesterday is being saved now.", Toast.LENGTH_LONG).show();
 
-        //Start the DataOrganizer.java class
-        Intent dataOrganizer = new Intent();
-        dataOrganizer.setClassName(context.getPackageName(), DataOrganizer.class.getName());
-        dataOrganizer.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(dataOrganizer);
+        // Set the alarm flag to 1, to indicate the condition that alarm was executed
+        SharedPreferences mSharedPreferences;
+        mSharedPreferences = context.getSharedPreferences("SaveData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor alarmEditor = mSharedPreferences.edit();
+        alarmEditor.putInt("AlarmSetFlag",1);
+        alarmEditor.apply();
+        //////////[ End of alarm flag ]///////////////////////////////////////////////////////
+
+        //Start the Mood.java to refresh the mood to Happy only if the app is ON
+        /**
+         * *****************boolean onStop onStart of Moods***************
+         * to check if the user is live or the app is off
+         * if it is off, it will be updated anyway on next launch
+         * if it is on "onStart/onCreate" it will relaunch Moods.java
+         */
+//        Intent reload = new Intent();
+//        dataOrganizer.setClassName(context.getPackageName(), DataOrganizer.class.getName());
+//        dataOrganizer.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        context.startActivity(dataOrganizer);
     }
 }
