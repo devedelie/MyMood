31969764100893cdd1e1863db25fc36fabf9706d
e210564e <devedelie@gmail.com>
2019-04-27 12:40:57 +0200

    - set the code back to where it was functioning , before testing a new method of history screen

--- app/src/main/java/com/elbaz/eliran/mymood/controller/HistoryFragments/StatisticSevenDaysAgo.java
+++ app/src/main/java/com/elbaz/eliran/mymood/controller/HistoryFragments/StatisticSevenDaysAgo.java
@@ -3,14 +3,15 @@ package com.elbaz.eliran.mymood.controller.HistoryFragments;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.elbaz.eliran.mymood.R;
@@ -30,8 +31,9 @@ public class StatisticSevenDaysAgo extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_statistic_seven_days_ago, container, false);
 
-        commentBtn = (ImageView) view.findViewById(R.id.fragment_comment_btn);
-        moodText = (TextView) view.findViewById(R.id.fragment_history_text);
+        colorBar = (ImageView) view.findViewById(R.id.color_bar_image_7);
+        commentBtn = (ImageView) view.findViewById(R.id.fragment_7_days_ago_comment_btn);
+        moodText = (TextView) view.findViewById(R.id.fragment_7_days_ago_text);
 
 
         SharedPreferences preferences = this.getActivity().getSharedPreferences("SaveData", Context.MODE_PRIVATE);
@@ -39,32 +41,48 @@ public class StatisticSevenDaysAgo extends Fragment {
         // Show the mood of specific day
         moodText.setText("7 days ago: \n"+value);
         // Check the mood and switch it to set the correct bar-color
-        ViewGroup.LayoutParams parameters = moodText.getLayoutParams();
         switch (value) {
             case "Super Happy Mood":
-                parameters.width = 1050;
-                moodText.setBackgroundColor(Color.YELLOW);
+                colorBar.setImageResource(R.drawable.super_happy_color);
+                FrameLayout.LayoutParams superHappy = new FrameLayout.LayoutParams(1000, 300);
+                superHappy.setMargins(0, 0, 0 ,0);
+                colorBar.setLayoutParams(superHappy);
                 break;
             case "Happy Mood!":
-                parameters.width = 1150;
-                moodText.setBackgroundColor(Color.GREEN);
+                colorBar.setImageResource(R.drawable.happy_color);
+                RelativeLayout.LayoutParams happy = new RelativeLayout.LayoutParams(800, 300);
+                happy.setMargins(0, 0, 0 ,0);
+                colorBar.setLayoutParams(happy);
+//                layout7DaysAgo = (LinearLayout) getActivity().findViewById(R.id.frame_7_days_ago);
+//                layout7DaysAgo.setWeightSum(30);
+//                LinearLayout.LayoutParams happy = new LinearLayout.LayoutParams(800, 250);
+//                happy.setMargins(0, 0, 0 ,0);
+//                layout7DaysAgo.setLayoutParams(happy);
+//                layout7DaysAgo.setBackgroundColor(Color.GREEN);
+
+
                 break;
             case "Normal Mood":
-                parameters.width = 650;
-                moodText.setBackgroundColor(Color.CYAN);
+                colorBar.setImageResource(R.drawable.normal_color);
+                FrameLayout.LayoutParams normal = new FrameLayout.LayoutParams(800, 300);
+                normal.setMargins(0, 0, 0 ,0);
+                colorBar.setLayoutParams(normal);
                 break;
             case "Disappointed":
-                parameters.width = 650;
-                moodText.setBackgroundColor(Color.GRAY);
+                colorBar.setImageResource(R.drawable.disappointed_color);
+                FrameLayout.LayoutParams disapp = new FrameLayout.LayoutParams(700, 300);
+                disapp.setMargins(0, 0, 0 ,0);
+                colorBar.setLayoutParams(disapp);
                 break;
             case "Sad Mood":
-                parameters.width = 650;
-                moodText.setBackgroundColor(Color.RED);
+                colorBar.setImageResource(R.drawable.sad_color);
+                FrameLayout.LayoutParams sad = new FrameLayout.LayoutParams(600, 300);
+                sad.setMargins(0, 0, 0 ,0);
+                colorBar.setLayoutParams(sad);
                 break;
             case "default":
-                moodText.setVisibility(View.INVISIBLE);
+                colorBar.setImageResource(R.drawable.no_color);
         }
-        moodText.setLayoutParams(parameters);
 
         // Show/hide the comment button by checking if comment was made for that day
 //        if (preferences.getString("comment7DaysAgo", "default").isEmpty() || preferences.getString("comment7DaysAgo", "default").equals("default")){
