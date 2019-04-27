6e50425fb79abaf40abc23eff9e506428d8b374e
e210564e <devedelie@gmail.com>
2019-04-27 18:33:33 +0200

    - Moved and erased old code functionality - MainActivity is used only for launching the Moods.java activity and to set the alarm

--- app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
+++ app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -18,8 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     // MOOD_REQUEST_CODE=4   -  Code number 4 to determine the Happy mood location on tasks(1-5)
     public static final int MOOD_REQUEST_CODE=1;
-    SharedPreferences mSharedPreferences;
-
 
     // Set time variables for repeated execution of DataOrganizer.java (with AlarmManager)
     int hour=23,minutes=59,seconds=0;
@@ -28,63 +25,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Set the alarm flag to 0, for testing only // should be commented/erased on production
-//        mSharedPreferences = getSharedPreferences("SaveData", Context.MODE_PRIVATE);
-//        SharedPreferences.Editor alarmEditor = mSharedPreferences.edit();
-//        alarmEditor.putInt("AlarmSetFlag",0);
-//        alarmEditor.apply();
-        //////////[ End of alarm flag test ]///////////////////////////////////////////////////////
-//
-        /**
-         * Periodic task:
-         * Daily silent-alarm for the system, to initialize and organize
-         * the moods and comments of the last 7 days. (with AlarmManager)
-         */
-        SharedPreferences result = getSharedPreferences("SaveData", Context.MODE_PRIVATE);
-        int flagValue = result.getInt("AlarmSetFlag", 0);
-        if(flagValue==0) {
-            setCalendarForAlarm();
-        }
-
-
-        // Launch mood screen
+        // Launch moods activity
         Intent defaultSmileyIntent = new Intent(this, Moods.class);
         startActivityForResult(defaultSmileyIntent, MOOD_REQUEST_CODE);
         overridePendingTransition(R.anim.no_change,R.anim.slide_up_info);
 
+        // Set the calender to prepare the periodic alarm
+        setCalendarForAlarm();
         finish();
-
-
-//            organizeData();
      }
-
-//     public void organizeData(){
-//        Intent data = new Intent(getApplicationContext(), DataOrganizer.class);
-//        startActivityForResult(data, MOOD_REQUEST_CODE);
-//         overridePendingTransition(R.anim.no_change,R.anim.slide_down_info);
-//    }
-
 
     /**
      * Set the calendar with date and time for system alarm to launch DataOrganizer.java
      */
     private void setCalendarForAlarm(){
-        // Set the alarm flag to 1, to indicate the condition on restart, that alarm is already ON
-        mSharedPreferences = getSharedPreferences("SaveData", Context.MODE_PRIVATE);
-        SharedPreferences.Editor alarmEditor = mSharedPreferences.edit();
-        alarmEditor.putInt("AlarmSetFlag",1);
-        alarmEditor.apply();
-        //////////[ End of alarm flag ]///////////////////////////////////////////////////////
-
         // Set the the time and date in calendar
         Calendar calendar = Calendar.getInstance();
-        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
-                hour,minutes,seconds);
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minutes);
+        calendar.set(Calendar.SECOND, seconds);
         // call setAlarm with the time in milli-seconds
         setAlarm(calendar.getTimeInMillis());
     }
-
-
     /**
      * setAlarm() method, will receive the time in milliseconds from setCalendarForAlarm()
      * and will initialize a repeating system alarm to call dataOrganizer.java
