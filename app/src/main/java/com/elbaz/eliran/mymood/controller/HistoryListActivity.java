140f4f77758c684416b189e6d44c8aa65f69712e
e210564e <devedelie@gmail.com>
2019-04-30 00:31:08 +0200

    - Added switch method to add data for each day of the week in the history - Next step: to implement conditions for comment data

--- app/src/main/java/com/elbaz/eliran/mymood/controller/HistoryListActivity.java
+++ app/src/main/java/com/elbaz/eliran/mymood/controller/HistoryListActivity.java
@@ -32,25 +32,63 @@ public class HistoryListActivity extends AppCompatActivity {
     }
 
     private void insertHistoryData(){
-        // FOR loop to set 7 days titles on history screen
-        for (int i = 7; i>=0; i--){
-            if (i == 1){
-                History history = new History();
-                history.setDayText("Yesterday"); // add text for yesterday item
-                mHistory.add(history);
-            }else if (i == 0){
-                History history = new History();
-                history.setDayText("Today"); // add text for today item
-                mHistory.add(history);
-            }else{
-                 History history = new History();
-                 history.setDayText(i + " Days ago"); // add text title for 7-2 days ago
-                 mHistory.add(history);
+
+        for (int x=7; x>=0 ; x-- ){
+            switch (x){
+                case 7:
+                    History history = new History();
+                    mHistory.add(new History("7 Days ago","comment bla bla",(R.drawable.ic_comment_black_48px)));
+                    break;
+                case 6:
+                    mHistory.add(new History("6 Days ago","comment bla bla",(R.drawable.ic_comment_black_48px)));
+                    break;
+                case 5:
+                    mHistory.add(new History("5 Days ago","comment bla bla",(R.drawable.ic_comment_black_48px)));
+                    break;
+                case 4:
+                    mHistory.add(new History("4 Days ago","comment bla bla",(R.drawable.ic_comment_black_48px)));
+                    break;
+                case 3:
+                    mHistory.add(new History("3 Days ago","comment bla bla",(R.drawable.ic_comment_black_48px)));
+                    break;
+                case 2:
+                    mHistory.add(new History("2 Days ago","",(R.drawable.ic_comment_black_48px)));
+                    break;
+                case 1:
+                    mHistory.add(new History("1 Day ago","comment bla bla",(R.drawable.ic_comment_black_48px)));
+                    break;
+                case 0:
+                    mHistory.add(new History("Today","comment bla bla",(R.drawable.ic_comment_black_48px)));
+                    break;
+
             }
+
         }
+
+        // FOR loop to set 7 days titles on history screen
+//        for (int i = 7; i>=0; i--){
+//            if (i == 1){
+//                History history = new History();
+////                history.setDayText("Yesterday"); // add text for yesterday item
+//                mHistory.add(new History("Yesterday","comment bla bla",(R.drawable.ic_comment_black_48px)));
+//            }else if (i == 0){
+//                History history = new History();
+////                history.setDayText("Today"); // add text for today item
+//                mHistory.add(new History("Today","",(R.drawable.ic_comment_black_48px)));
+//            }else{
+//                 History history = new History();
+//                 history.setDayText(i + " Days ago"); // add text title for 7-2 days ago
+//                 mHistory.add(history);
+//            }
+//        }
+
+
         // Important: Need to call notifyDataSetChanged(), otherwise no data will be shown on the list
         mHistoryRecyclerAdapter.notifyDataSetChanged();
     }
+
+
+
 
     private void initRecyclerView(){
         // Setting a LinearLayout manager
@@ -70,4 +108,5 @@ public class HistoryListActivity extends AppCompatActivity {
         // set the adapter to the recyclerView
         mRecyclerView.setAdapter(mHistoryRecyclerAdapter);
     }
+
 }
