72597facc032b40975807c9605f38378c09fb91d
e210564e <devedelie@gmail.com>
2019-05-03 16:59:06 +0200

    - Added a condition to re-activate the alarm, in case when the device is restarted

--- app/src/main/java/com/elbaz/eliran/mymood/model/DataOrganizeTaskLauncher.java
+++ app/src/main/java/com/elbaz/eliran/mymood/model/DataOrganizeTaskLauncher.java
@@ -1,14 +1,23 @@
 package com.elbaz.eliran.mymood.model;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * Created by Eliran Elbaz on 17-Apr-19.
  */
 public class DataOrganizeTaskLauncher extends BroadcastReceiver {
+    SharedPreferences mSharedPreferences;
+    private AlarmManager mAlarmManager;
+    private PendingIntent mPendingIntent;
+    int hour=0, minutes=0;
 
     // This method will be invoked once the AlarmManager reaches the time we set for data saving
     @Override
@@ -21,6 +30,28 @@ public class DataOrganizeTaskLauncher extends BroadcastReceiver {
         dataOrganizer.setClassName(context.getPackageName(), DataOrganizer.class.getName());
         dataOrganizer.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(dataOrganizer);
+
+        // If the device has been rebooted, this condition will set the alarm back to cycle.
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            // set alarm flag
+            mSharedPreferences = context.getSharedPreferences("Data", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putString("AlarmSetFlag", "Standby").commit();
+            // Initialize AlarmManager
+            mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent alarm = new Intent(context, DataOrganizeTaskLauncher.class);
+            mPendingIntent = PendingIntent.getBroadcast(context, 0, alarm,0);
+            // Set the time in calendar for the first launch
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minutes);
+            // setRepeating() will specify a precise custom interval of repeats every 24H
+            mAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    1000 * 60 * 60 * 24, mPendingIntent);
+            Toast.makeText(context, "Alarm set", Toast.LENGTH_LONG).show();
+        }
+
 
         // Set the alarm flag to OFF, for testing only
 //        SharedPreferences mSharedPreferences;
