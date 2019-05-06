f69cb87affc18893f91d8446941764a03afb4ae2
e210564e <devedelie@gmail.com>
2019-05-06 18:21:49 +0200

    - Created a constants class to store final variables

---
+++ app/src/main/java/com/elbaz/eliran/mymood/model/Constants.java
@@ -0,0 +1,13 @@
+package com.elbaz.eliran.mymood.model;
+
+/**
+ * This class made to store constant variables
+ */
+public class Constants {
+    public static final String[] emailSubjects = {"Subject: Hey, I'm sad and I wanted to share it with you.",
+            "Subject: Hey, I'm in disappointed-mood and I wanted to share it with you.",
+            "Subject: Hey, today I feel normal and I wanted to share it with you.",
+            "Subject: Hey, I'm in Happy-mood and I wanted to share it with you.",
+            "Subject: Hey, today I'm in a super-happy-mood and I wanted to share it with you."};
+
+}
