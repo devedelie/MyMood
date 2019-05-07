a6980abbfff2b96f3a1d414097a2d7bacf65ccb6
e210564e <devedelie@gmail.com>
2019-05-07 22:04:02 +0200

    - Recreated the method checkDay() from scratch to calculate the difference between the current date and the last time when DataOrganizer was launched.  Then it invokes the organizer the times when the app was off.

--- app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
+++ app/src/main/java/com/elbaz/eliran/mymood/controller/MainActivity.java
@@ -1,9 +1,9 @@
 package com.elbaz.eliran.mymood.controller;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.elbaz.eliran.mymood.R;
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int MOOD_REQUEST_CODE=1;
     private static MainActivity instance;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,27 +35,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Check if and how many days has passed, and update history data regardless
+     * Check if and how many days has passed since last time the data was organized, and update history data regardless
      */
     public void CheckDay() {
-        // Get today's date from calendar
-         Calendar c = Calendar.getInstance();
-         int thisDay = c.get(Calendar.DAY_OF_YEAR);
-         long todayMillis = c.getTimeInMillis();
-         // get the last date
-         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-         long last = prefs.getLong("DateComparator", 0);
-         c.setTimeInMillis(last);
-         int lastDay = c.get(Calendar.DAY_OF_YEAR);
+        prefs = getSharedPreferences("Data", Context.MODE_PRIVATE);
+        // Check if it is the first launch of the app since last time DataOrganizer was launched
+        boolean isFirstLaunch = prefs.getBoolean("IsFirstLaunch" , true);
+        if (isFirstLaunch) {
+            Calendar firstLaunchDate = Calendar.getInstance();
+            int yearOfLaunch = Calendar.getInstance().get(Calendar.YEAR);
+            int monthOfLaunch = Calendar.getInstance().get(Calendar.MONTH);
+            int dayOfLaunch = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+            firstLaunchDate.set(yearOfLaunch,++monthOfLaunch,dayOfLaunch);
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putInt("FirstLaunchYear", yearOfLaunch);
+            edit.putInt("FirstLaunchMonth", monthOfLaunch);
+            edit.putInt("FirstLaunchDay", dayOfLaunch);
+            edit.putBoolean("IsFirstLaunch" , false);
+            edit.commit();
+        }
+        Calendar dateLoad = Calendar.getInstance();
+        // Load the first launch date
+        int year1 = prefs.getInt("FirstLaunchYear",0);
+        int month1 = (prefs.getInt("FirstLaunchMonth",0)) ;
+        int day1 = prefs.getInt("FirstLaunchDay",0);
+        //Add first launch date into Calendar
+        dateLoad.set(year1,month1,day1);
+        long firstLaunchTime = dateLoad.getTimeInMillis();
 
-         if( last==0 || lastDay != thisDay ){
-             //New day, update TextView and preference:
-             SharedPreferences.Editor edit = prefs.edit();
-             edit.putLong("DateComparator", todayMillis);
-             edit.commit();
-             // Launch DataOrganizer.class
-                 Intent defaultSmileyIntent = new Intent(this, DataOrganizer.class);
-                 startActivityForResult(defaultSmileyIntent, MOOD_REQUEST_CODE);
-         }
-     }
+        // Create today's calender instance
+        Calendar cal2 = Calendar.getInstance();
+        // Set the date for calendar instance
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        cal2.set(year, ++month, day); // ++months -->  because January starts with 0
+        // Get the represented date in milliseconds
+        long todaysTime = cal2.getTimeInMillis();
+
+        // Calculate difference between last launch of DataOrganizer.java
+        long diff = todaysTime - firstLaunchTime;
+
+        // Calculate difference in days
+        long diffDays = diff / (24 * 60 * 60 * 1000);
+
+        if (diffDays !=0){
+            for (long x = diffDays; x > 0 ; x--){
+                Intent defaultSmileyIntent = new Intent(this, DataOrganizer.class);
+            startActivityForResult(defaultSmileyIntent, MOOD_REQUEST_CODE);
+            }
+        }
+
+    }
 }
+
