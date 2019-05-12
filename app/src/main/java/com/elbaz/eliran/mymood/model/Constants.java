b137f4ca73d092e84acbf1b296a0f1ce69293c9d
e210564e <devedelie@gmail.com>
2019-05-12 16:49:29 +0200

    - Modified email subject

--- app/src/main/java/com/elbaz/eliran/mymood/model/Constants.java
+++ app/src/main/java/com/elbaz/eliran/mymood/model/Constants.java
@@ -4,11 +4,11 @@ package com.elbaz.eliran.mymood.model;
  * This class made to store constant variables
  */
 public class Constants {
-    public static final String[] emailSubjects = {"Sujet: Hey, je suis triste et je voulais partager avec toi.",
-            "Sujet: Hey, je suis déçu et je voulais le partager avec toi.",
-            "Sujet: Hey, aujourd'hui, je me sens normal et je voulais le partager avec toi.",
-            "Sujet: Hey, je suis de bonne humeur et je voulais le partager avec toi.",
-            "Sujet: Hey, aujourd'hui, je suis de très bonne humeur et je voulais le partager avec toi."};
+    public static final String[] emailSubjects = {"Hey, je suis triste et je voulais partager avec toi.",
+            "Hey, je suis déçu et je voulais le partager avec toi.",
+            "Hey, aujourd'hui, je me sens normal et je voulais le partager avec toi.",
+            "Hey, je suis de bonne humeur et je voulais le partager avec toi.",
+            "Hey, aujourd'hui, je suis de très bonne humeur et je voulais le partager avec toi."};
 
     public static final  String dataNotAvailable = "N / A (aucune donnée n'a encore été enregistrée)";
 
