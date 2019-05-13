40ddb6b1b731b53d5d1cf351fd4e34e2c6c298cd
e210564e <devedelie@gmail.com>
2019-05-13 18:24:19 +0200

    - Updated new constants to loop through while loading the history with SharedPreferences

--- app/src/main/java/com/elbaz/eliran/mymood/model/Constants.java
+++ app/src/main/java/com/elbaz/eliran/mymood/model/Constants.java
@@ -1,5 +1,7 @@
 package com.elbaz.eliran.mymood.model;
 
+import com.elbaz.eliran.mymood.R;
+
 /**
  * This class made to store constant variables
  */
@@ -12,7 +14,14 @@ public class Constants {
 
     public static final  String dataNotAvailable = "N / A (aucune donnée n'a encore été enregistrée)";
 
+    public static final String[] moodDaysAgo = {"7DaysAgo","6DaysAgo","5DaysAgo","4DaysAgo","3DaysAgo","2DaysAgo","1DaysAgo"};
+
+    public static final String[] commentDaysAgo = {"comment7DaysAgo","comment6DaysAgo","comment5DaysAgo","comment4DaysAgo","comment3DaysAgo","comment2DaysAgo","comment1DaysAgo"};
+
     public static final String[] historyDaysText = {"Il y a une semaine", "Il y a six jours", "Il y a cinq jours", "Il y a quatre jours","Il y a trois jours", "Avent-hier", "Hier"};
+
+    public static final int[] historyColors = {R.color.SadSmiley, R.color.DisappointedSmiley, R.color.NormalSmiley,
+            R.color.HappySmiley, R.color.SuperHappySmiley};
     
 
 }
